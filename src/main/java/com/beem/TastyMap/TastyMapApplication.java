2e36c53e796dc03402cd054bad746d01578ea27a
Beyzatkc <beyzatakca@gmail.com>
2026-01-13 12:09:25 +0300

    baslangic

---
+++ src/main/java/com/beem/TastyMap/TastyMapApplication.java
@@ -0,0 +1,13 @@
+package com.beem.TastyMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastyMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TastyMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beem/TastyMap/TastyMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beem.TastyMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastyMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
